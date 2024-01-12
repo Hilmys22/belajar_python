bfaf55b7ec4f563f5b26895a500cd68cbf13b02f
Hilmys22 <ahmadhilmys22@gmail.com>
2024-01-12 23:35:14 +0700

    progres ke

---
+++ main15.1.py
@@ -0,0 +1,27 @@
+# format string
+
+# contoh generic
+# string
+nama = "ahmad"
+format_str = f"hai {nama}"
+print(format_str)
+
+# boolean
+bool = True
+format_str = f"boolean = {bool}"
+print(format_str)
+
+# angka
+angka = 1965.5
+format_str = f"angka = {angka}"
+print(format_str)
+
+# bilangan bulat
+angka = 18
+format_str = f"bilangan bulat = {angka:d}"
+print(format_str)
+
+# bilangan ribuan
+angka = 8000000000
+format_str = f"ribuan = {angka:,}"
+print(format_str)
